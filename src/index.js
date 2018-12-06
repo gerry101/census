67e47202fe795330d235a1ae43b2b63c23c206e4
Gerry <gerrymigwi6@gmail.com>
2018-12-06 23:12:29 +0200

    Added the first reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import reducer from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
